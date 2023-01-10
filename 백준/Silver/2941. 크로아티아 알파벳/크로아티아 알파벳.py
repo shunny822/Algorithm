10541cdd7dcdec8f4731e2a3468b8d849638f060
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-01-10 16:44:10 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,7 +2,7 @@ word = input()
 croa = ['dz=', 'c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
 
 for a in croa:
-    while a in word:
+    if a in word:
         word = word.replace(a, '1')
 
 print(len(word))
