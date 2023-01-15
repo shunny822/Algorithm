3ecc7e4132e5458a905ed325541b999cdb66e03f
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-01-15 15:04:34 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 72 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,14 @@
+t = int(input())
+for i in range(t):
+    k = int(input())
+    n = int(input())
+    floor = list(range(1, n+1))
+    for j in range(k):
+        next_floor = []
+        for l in range(n):
+            if l == 0:
+                next_floor.append(1)
+            else:
+                next_floor.append(next_floor[-1]+floor[l])
+        floor = next_floor
+    print(floor[-1])
