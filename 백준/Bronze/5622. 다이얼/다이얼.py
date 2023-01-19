8c505b71f39d042169c630a002f58e71ebbf7c32
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-01-19 14:18:35 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,8 +1,11 @@
-alph = {'A': 3, 'B': 3, 'C': 3, 'D': 4, 'E': 4, 'F': 4, 'G': 5, 'H': 5, 'I': 5,
-        'J': 6, 'K': 6, 'L': 6, 'M': 7, 'N': 7, 'O':7, 'P': 8, 'Q': 8, 'R': 8,
-        'S': 8, 'T': 9, 'U': 9, 'V': 9, 'W': 10, 'X': 10, 'Y': 10, 'Z': 10}
-string = input()
+dial = {'ABC': 3, 'DEF': 4, 'GHI': 5, 'JKL': 6, 'MNO': 7,
+        'PQRS': 8, 'TUV': 9, 'WXYZ': 10}
+word = list(input())
 time = 0
-for s in string:
-    time += alph[s]
+
+for a in dial:
+    for w in word:
+        if w in a:
+            time += dial[a]
+
 print(time)
