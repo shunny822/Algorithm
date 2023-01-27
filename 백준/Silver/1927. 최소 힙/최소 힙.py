6141a92bd05a68b1678838af64d4deabd79cf529
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-01-27 21:41:22 +0900

    [Silver II] Title: 최소 힙, Time: 124 ms, Memory: 36984 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+n = int(input())
+array = []
+for _ in range(n):
+    x = int(input())
+    if x > 0:
+        heapq.heappush(array, x)
+    else:
+        print(heapq.heappop(array) if array else 0)
