23b6900423bbf0f3675f39f560af9037aabbd946
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-01-29 22:07:42 +0900

    [Silver V] Title: 국회의원 선거, Time: 44 ms, Memory: 32876 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+n = int(input())
+dasom = int(input())
+candidate = [-int(input()) for _ in range(n-1)]
+heapq.heapify(candidate)
+cnt = 0
+
+if candidate:
+    while -candidate[0] >= dasom:
+        heapq.heappush(candidate, heapq.heappop(candidate)+1)
+        dasom += 1
+        cnt += 1
+    print(cnt)
+else:
+    print(0)
