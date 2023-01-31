f08f06536a352ca1a9435c2f45f29f2989a36272
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-01 00:02:28 +0900

    [Silver V] Title: 올림픽, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,16 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+record = {}
+for _ in range(n):
+    nation, g, s, b = map(int, sys.stdin.readline().split())
+    record[nation] = (g, s, b)
+
+medals = sorted(list(record.values()), reverse=True)
+rank = 0
+for i in range(len(medals)):
+    if medals[i] == record[k]:
+        rank = i+1
+        break
+    
+print(rank)
