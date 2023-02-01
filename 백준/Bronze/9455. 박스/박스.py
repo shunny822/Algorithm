fe5dd8eda3ed3ca06a86e595c839bd4136a735d1
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-01 09:35:39 +0900

    [Bronze I] Title: 박스, Time: 152 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9455. 박스/박스.py
+++ 백준/Bronze/9455. 박스/박스.py
@@ -4,16 +4,11 @@ t = int(input())
 for _ in range(t):
     m, n = map(int, input().split())
     matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
-    turn_matrix = [[0]*m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            turn_matrix[i][j] = matrix[j][i]
-
     total = 0
     for i in range(n):
         cnt = 0
-        for j in range(-1,-(m+1),-1):
-            if turn_matrix[i][j]:
+        for j in reversed(range(m)):
+            if matrix[j][i]:
                 total += cnt
             else:
                 cnt += 1
