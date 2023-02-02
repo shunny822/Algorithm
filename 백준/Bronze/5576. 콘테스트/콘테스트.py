e6f9b5ec4e94b6f04780c985f003123e821af0d5
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-02 14:23:37 +0900

    [Bronze II] Title: 콘테스트, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5576. 콘테스트/콘테스트.py
+++ 백준/Bronze/5576. 콘테스트/콘테스트.py
@@ -1,22 +1,5 @@
 import sys
-import heapq
 
-w = []
-k = []
-for i in range(10):
-    if i < 3:
-        heapq.heappush(w, int(sys.stdin.readline()))
-    else:
-        n = int(sys.stdin.readline())
-        if n > w[0]:
-            heapq.heapreplace(w, n)
-
-for i in range(10):
-    if i < 3:
-        heapq.heappush(k, int(sys.stdin.readline()))
-    else:
-        n = int(sys.stdin.readline())
-        if n > k[0]:
-            heapq.heapreplace(k, n)
-
-print(sum(w), sum(k))
+w = sorted([int(sys.stdin.readline()) for _ in range(10)])
+k = sorted([int(sys.stdin.readline()) for _ in range(10)])
+print(sum(w[-3:]), sum(k[-3:]))
