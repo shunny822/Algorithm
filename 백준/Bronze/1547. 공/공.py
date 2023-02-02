dcff3f8df233cc2742ab5f161a2ab95fefdf64f8
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-02 14:00:30 +0900

    [Bronze III] Title: 공, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.py
+++ 백준/Bronze/1547. 공/공.py
@@ -1,9 +1,9 @@
 import sys
 
 m = int(input())
-cups = {1: 0, 2: 1, 3: 2}
+cups = {'1': 0, '2': 1, '3': 2}
 for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = sys.stdin.readline().split()
     cups[a], cups[b] = cups[b], cups[a]
 
 for k in cups:
