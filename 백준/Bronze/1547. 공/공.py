3be322f4cb208d7b844a53ca8ff01ee836b3afd3
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-02 13:57:11 +0900

    [Bronze III] Title: 공, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.py
@@ -0,0 +1,11 @@
+import sys
+
+m = int(input())
+cups = {1: 0, 2: 1, 3: 2}
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    cups[a], cups[b] = cups[b], cups[a]
+
+for k in cups:
+    if cups[k] == 0:
+        print(k)
