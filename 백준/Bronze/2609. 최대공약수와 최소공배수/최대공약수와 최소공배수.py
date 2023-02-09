c3d29cef9dc1f053a502ac320974506ca4b4a407
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-09 22:13:39 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,22 +1,15 @@
-a, b = map(int, input().split())
-A = set()
-B = set()
-
-for i in range(1, int(a**(1/2))+1):
-    if a % i == 0:
-        A.add(a//i)
-        A.add(i)
-for i in range(1, int(b**(1/2))+1):
-    if b % i == 0:
-        B.add(b//i)
-        B.add(i)
-print(max(A&B))
-
-x = a
-y = b
-while x != y:
-    if x < y:
-        x += a
+def common_d(a, b):
+    if a % b == 0:
+        return b
     else:
-        y += b
-print(x)
+        return common_d(b, a % b)
+
+a, b = map(int, input().split())
+print(common_d(a, b))
+x, y = a, b
+while a != b:
+    if a < b:
+        a += x
+    else:
+        b += y
+print(a)
