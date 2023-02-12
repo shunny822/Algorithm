fc27a70b4caad7c85652dc97032fd154476eff16
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-12 22:16:29 +0900

    [Silver II] Title: 색종이 만들기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,32 @@
+import sys
+
+def what_color(copy_p):
+    l = len(copy_p)
+    is_differ = False
+
+    for y in range(l):
+        for x in range(l):
+            if copy_p[y][x] != copy_p[0][0]:
+                is_differ = True
+                break
+        if is_differ:
+            break
+    else:
+        if copy_p[0][0]:
+            blue.append(1)
+        else:
+            white.append(1)
+        return
+
+    if is_differ:
+        for i in range(0, l, l//2):
+            for j in range(0, l, l//2):
+                div_p = [copy_p[k][j:j+l//2] for k in range(i, i+l//2)]
+                what_color(div_p)
+
+n = int(input())
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+white = []
+blue = []
+what_color(paper)
+print(sum(white), sum(blue), sep='\n')
