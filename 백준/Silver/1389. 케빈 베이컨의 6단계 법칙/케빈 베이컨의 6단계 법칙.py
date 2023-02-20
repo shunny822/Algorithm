da020ad91524e97a667707bc869dcad116a68f48
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-02-21 00:54:41 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 68 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -3,35 +3,33 @@ input = sys.stdin.readline
 from collections import deque
 
 def bfs(a, b):
-    q = deque([a])
+    q = deque([(a, 0)])
     check[a] = True
-    cnt = {a: 0}
 
     while q:
-        x = q.popleft()
-        for friend in friends[x]:
-            if friend == b:
-                friends_cnt[a] += cnt[x] + 1
-                friends_cnt[b] += cnt[x] + 1
-                return
-            elif not check[friend]:
-                check[friend] = True
-                q.append(friend)
-                cnt[friend] = cnt[x] + 1
+        x, cnt = q.popleft()
+        if x == b:
+            f_cnt[a] += cnt
+            f_cnt[b] += cnt
+            return
+        
+        for f in friends[x]:
+            if not check[f]:
+                check[f] = True
+                q.append((f, cnt+1))
 
 n, m = map(int, input().split())
-friends = [[] for _ in range(n+1)]
-friends_cnt = [0 for _ in range(n+1)]
+friends = [[] for _ in range(n)]
+f_cnt = [0 for _ in range(n)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    friends[a].append(b)
-    friends[b].append(a)
+    friends[a-1].append(b-1)
+    friends[b-1].append(a-1)
 
-for i in range(1, n):
-    for j in range(i+1, n+1):
-        check = [False] * (n+1)
+for i in range(n-1):
+    for j in range(i+1, n):
+        check = [False] * (n)
         bfs(i, j)
 
-friends_cnt[0] = n * n
-print(friends_cnt.index(min(friends_cnt)))
+print(f_cnt.index(min(f_cnt))+1)
