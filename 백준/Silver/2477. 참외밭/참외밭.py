2555f14460debd448c4eac8b3577de826c572130
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-03-01 21:36:09 +0900

    [Silver II] Title: 참외밭, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,29 +1,8 @@
 import sys
 intput = sys.stdin.readline
-from collections import deque
 
 n = int(input())
-farm = deque()
-h = 0
-w = 0
-for _ in range(6):
-    a, b = map(int, input().split())
-    farm.append((a, b))
-    if a <= 2 and b > w:
-        w = b
-    elif a > 2 and b > h:
-        h = b
-
-for i in range(6):
-    if (farm[i-1][1] == h and farm[i][1] == w) or (farm[i-1][1] == w and farm[i][1] == h):
-        if i < 2:
-            while i > -1:
-                farm.append(farm.popleft())
-                i -= 1
-        else:
-            while i < 5:
-                farm.appendleft(farm.pop())
-                i += 1
-        break
-
-print(n * (h * w - (farm[1][1] * farm[2][1])))
+farm = [tuple(map(int, input().split())) for _ in range(6)]
+for i in reversed(range(6)):
+    if farm[i][0] == farm[i-2][0] and farm[i-1][0] == farm[i-3][0]:
+        print(n * (farm[i-4][1] * farm[i-5][1] - (farm[i-1][1] * farm[i-2][1])))
