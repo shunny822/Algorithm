945ab855c254f5c2aff036c01e7581daf91d10a8
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-03-15 23:20:35 +0900

    [Silver II] Title: 나무꾼 이다솜, Time: 316 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
@@ -2,21 +2,24 @@ import sys
 input = sys.stdin.readline
 
 n, c, w = map(int, input().split())
-length = [int(input()) for _ in range(n)]
-max_money = 0
+length = sorted([int(input()) for _ in range(n)])
+max_money = length[-1] * w
 
-for x in range(1, max(length)+1):
-    money = 0
-    for l in length:
-        d, m = divmod(l, x)
-        if m == 0:
-            if x*d*w - c*(d-1) > 0:
-                money += x*d*w - c*(d-1)
-        else:
-            if x*d*w - c*d > 0:
-                money += x*d*w - c*d
+if n == 1:
+    print(max_money)
+else:
+    for x in range(1, length[-2]+1):
+        money = 0
+        for l in length:
+            d, m = divmod(l, x)
+            if m == 0:
+                if x*d*w - c*(d-1) > 0:
+                    money += x*d*w - c*(d-1)
+            else:
+                if x*d*w - c*d > 0:
+                    money += x*d*w - c*d
 
-    if money > max_money:
-        max_money = money
+        if money > max_money:
+            max_money = money
 
-print(max_money)
+    print(max_money)
