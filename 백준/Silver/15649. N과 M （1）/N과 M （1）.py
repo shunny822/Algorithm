38dfcacd4d818dec54bda90c2a6ec378aa68852e
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-03-16 23:09:15 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,5 @@
-def generator(ck, combi=list):
-    if len(combi) == m:
-        return print(*combi)
-    for i in range(1, n+1):
-        if not ck[i]:
-            ck[i] = True
-            combi.append(i)
-            generator(ck, combi)
-            ck[i] = False
-            combi.pop()
+import itertools
 
 n, m = map(int, input().split())
-check = [False] * (n+1)
-check[0] = True
-combi_n = []
-generator(check, combi_n)
+for combi in itertools.permutations(range(1, n+1), m):
+    print(*combi)
