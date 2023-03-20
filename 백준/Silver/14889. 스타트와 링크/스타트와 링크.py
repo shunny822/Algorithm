8a77f57205491cea5b69a2a4611ac3a441e02486
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-03-20 20:59:02 +0900

    [Silver II] Title: 스타트와 링크, Time: 2504 ms, Memory: 37960 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,23 +1,31 @@
 import sys
 input = sys.stdin.readline
-import itertools
+
+def team_maker(check, s):
+    if sum(check) == n//2:
+        team_a = [i for i in range(n) if check[i]]
+        team_b = [i for i in range(n) if not check[i]]
+        a = b = 0
+
+        for i in team_a:
+            for j in team_a:
+                a += stat[i][j]
+        for i in team_b:
+            for j in team_b:
+                b += stat[i][j]
+
+        result.append(abs(a - b))
+        return
+
+    for i in range(s, n):
+        if not check[i]:
+            check[i] = True
+            team_maker(check, i+1)
+            check[i] = False
 
 n = int(input())
 stat = [list(map(int, input().split())) for _ in range(n)]
-total = set(range(n))
-result = 1e9
-
-for team_a in itertools.combinations(range(n), n//2):
-    team_b = total - set(team_a)
-    a = b = 0
-    for i in team_a:
-        for j in team_a:
-            a += stat[i][j]
-    for i in team_b:
-        for j in team_b:
-            b += stat[i][j]
-
-    if abs(a - b) < result:
-        result = abs(a - b)
-
-print(result)
+is_used = [False] * n
+result = []
+team_maker(is_used, 0)
+print(min(result))
