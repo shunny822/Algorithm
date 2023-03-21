efb7849d1927e89316a1c1b3cf55226c2beb4a73
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-03-21 15:51:14 +0900

    [Silver I] Title: 에너지 모으기, Time: 116 ms, Memory: 32620 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -1,25 +1,25 @@
 import sys
 input = sys.stdin.readline
-import itertools
+
+def energe(check, total):
+    if sum(check) == n-2:
+        result.append(total)
+        return
+    
+    for i in range(1, n-1):
+        if not check[i]:
+            check[i] = True
+            a = b = i
+            while check[a]:
+                a -= 1
+            while check[b]:
+                b += 1
+            energe(check, total + balls[a] * balls[b])
+            check[i] = False
 
 n = int(input())
 balls = list(map(int, input().split()))
-result = 0
-
-for combi in itertools.permutations(range(1, n-1), n-2):
-    is_used = [False] * n
-    total = 0
-
-    for i in combi:
-        is_used[i] = True
-        a = b = i
-        while is_used[a]:
-            a -= 1
-        while is_used[b]:
-            b += 1
-        total += balls[a] * balls[b]
-    
-    if total > result:
-        result = total
-
-print(result)
+is_used = [False] * n
+result = []
+energe(is_used, 0)
+print(max(result))
