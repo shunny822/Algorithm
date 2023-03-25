daa720a6e72cac8dca198812664df2e097febef5
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-03-25 21:36:06 +0900

    [Silver II] Title: 나무 자르기, Time: 492 ms, Memory: 113752 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -4,9 +4,9 @@ input = sys.stdin.readline
 def cut(start, end):
     mid_h = (end + start) // 2
     mid = 0
-    for tree in trees:
+    for tree, num in trees.items():
         if tree > mid_h:
-            mid += tree - mid_h
+            mid += (tree - mid_h) * num
     
     if mid == m or start == mid_h:
         return print(mid_h)
@@ -14,7 +14,10 @@ def cut(start, end):
         cut(start, mid_h)
     else:
         cut(mid_h, end)
-    
+
 n, m = map(int, input().split())
-trees = list(map(int, input().split()))
+trees = {}
+for h in map(int, input().split()):
+    trees[h] = trees.get(h, 0) + 1
+
 cut(0, max(trees))
