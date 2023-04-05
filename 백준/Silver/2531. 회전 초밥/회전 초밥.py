e19bf11405662f4314a5d2c062f2ecf75f6df432
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-04-05 21:09:37 +0900

    [Silver I] Title: 회전 초밥, Time: 76 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -8,10 +8,7 @@ nums = {}
 for i in range(-k, 0):
     nums[sushi[i]] = nums.get(sushi[i], 0) + 1
 
-if c in nums:
-    ans = len(nums)
-else:
-    ans = len(nums) + 1
+ans = len(nums) if c in nums else len(nums) + 1
 
 for j in range(n):
     if nums[sushi[j-k]] == 1:
@@ -20,10 +17,6 @@ for j in range(n):
         nums[sushi[j-k]] -= 1
 
     nums[sushi[j]] = nums.get(sushi[j], 0) + 1
-
-    if c in nums:
-        ans = max(ans, len(nums))
-    else:
-        ans = max(ans, len(nums) + 1)
+    ans = max(ans, len(nums)) if c in nums else max(ans, len(nums) + 1)
 
 print(ans)
