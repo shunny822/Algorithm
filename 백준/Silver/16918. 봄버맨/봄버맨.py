692ddcbc58c923ea15be50726acbbb9693d5480c
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-04-19 02:42:24 +0900

    [Silver I] Title: 봄버맨, Time: 4088 ms, Memory: 31504 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -14,11 +14,10 @@ for i in range(r):
 
 n -= 1
 for t in range(n):
+    for i in range(r):
+        for j in range(c):
+            board[i][j] += 1
     if t & 1:
-        for i in range(r):
-            for j in range(c):
-                if board[i][j] > 0:
-                    board[i][j] += 1
         check = [[False] * c for _ in range(r)]
         for i in range(r):
             for j in range(c):
@@ -34,11 +33,6 @@ for t in range(n):
             for j in range(c):
                 if check[i][j]:
                     board[i][j] = 0
-
-    else:
-        for i in range(r):
-            for j in range(c):
-                board[i][j] += 1
 
 for i in range(r):
     for j in range(c):
