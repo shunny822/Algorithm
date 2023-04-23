24b741f75ab5e57fa581fe1b78935c079ec39445
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-04-24 00:25:35 +0900

    [Gold IV] Title: 여행 가자, Time: 664 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,21 +1,19 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def bfs(start, end, visited):
-    q = deque([start])
+def dfs(start, end, visited):
+    stack = [start]
     visited[start] = True
 
-    while q:
-        x = q.popleft()
+    while stack:
+        x = stack.pop()
         if x == end:
             return True
         for j in range(n):
             if matrix[x][j] and not visited[j]:
-                q.append(j)
+                stack.append(j)
                 visited[j] = True
     return False
-
 
 n = int(input())
 m = int(input())
@@ -24,7 +22,7 @@ route = list(map(int, input().split()))
 
 for i in range(m-1):
     check = [False] * n
-    is_possible = bfs(route[i]-1, route[i+1]-1, check)
+    is_possible = dfs(route[i]-1, route[i+1]-1, check)
     if not is_possible:
         print('NO')
         break
