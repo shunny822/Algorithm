47132235809592701ce3d31d0574d2d0ed316a90
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-05-11 00:34:19 +0900

    [Gold V] Title: A와 B, Time: 44 ms, Memory: 31364 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+s = input().rstrip()
+l = len(s)
+T = input().rstrip()
+res = []
+
+def plus(t):
+    if len(t) == l:
+        if t == s:
+            res.append(1)
+        return
+    if t[-1] == 'A':
+        plus(t[:-1])
+    if t[-1] == 'B':
+        plus(t[-2::-1])
+
+plus(T)
+print(1 if res else 0)
