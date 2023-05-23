7211953bb190c0020fb5407712c25dbf19efbf81
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-05-23 21:42:39 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,2 +1,19 @@
-s = input()
-print('0'*(s.count('0')//2) + '1'*(s.count('1')//2))
+S = list(input())
+zero = S.count('0')//2
+one = S.count('1')//2
+z_cnt = o_cnt = 0
+ans = ''
+
+for s in S:
+    if s == '0':
+        if z_cnt < zero:
+            ans += s
+            z_cnt += 1
+        else:
+            continue
+    else:
+        if o_cnt >= one:
+            ans += s
+        else:
+            o_cnt += 1
+print(ans)
