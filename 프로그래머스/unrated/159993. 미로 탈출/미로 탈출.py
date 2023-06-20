d552ce9bd96311b54b3cd66456b72435b806784b
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-06-20 19:24:37 +0900

    [unrated] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def solution(maps):
+    def bfs(start, maze=list, target=str):
+        check = [[False]*len(maps[0]) for _ in range(len(maze))]
+        q = deque([(start[0], start[1], 0)])
+        check[start[0]][start[1]] = True
+
+        while q:
+            y, x, turn = q.popleft()
+            if maze[y][x] == target:
+                return turn
+            for dy, dx in delta:
+                ny = y + dy
+                nx = x + dx
+                if 0 <= ny < len(maze) and 0 <= nx < len(maze[0]) and not check[ny][nx] and maze[ny][nx] != 'X':
+                    check[ny][nx] = True
+                    q.append((ny, nx, turn+1))
+        else:
+            return -1
+
+    s = (0, 0)
+    l = (0, 0)
+    delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for i, line in enumerate(maps):
+        if 'S' in line:
+            s = (i, line.index('S'))
+            break
+    for i, line in enumerate(maps):
+        if 'L' in line:
+            l = (i, line.index('L'))
+            break
+    
+    step1 = bfs(s, maps, 'L')
+    if step1 == -1:
+        return -1
+    
+    step2 = bfs(l, maps, 'E')
+    if step2 == -1:
+        return -1
+
+    return step1 + step2
