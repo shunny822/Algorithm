d719f4ed8cfef962f4a7b97d8c9f4708570324ec
Suhyun Choe <113580033+shunny822@users.noreply.github.com>
2023-10-23 15:42:45 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 620 ms, Memory: 70272 KB -BaekjoonHub

--- 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
@@ -2,22 +2,14 @@ import sys
 input = sys.stdin.readline
 
 r, c, q = map(int, input().split())
-picture = []
-for _ in range(r):
-    px = list(map(int, input().split()))
-    cnt = 0
-    line = []
-    for i in range(c):
-        cnt += px[i]
-        line.append(cnt)
-    picture.append(line)
+picture = [[0] * (c+1)]
+for i in range(r):
+    picture.append([0] + list(map(int, input().split())))
+
+for i in range(1, r+1):
+    for j in range(1, c+1):
+        picture[i][j] += picture[i-1][j] + picture[i][j-1] - picture[i-1][j-1]
 
 for _ in range(q):
     r1, c1, r2, c2 = map(int, input().split())
-    avg = 0
-    for i in range(r1-1, r2):
-        if c1 == 1:
-            avg += picture[i][c2-1]
-        else:
-            avg += picture[i][c2-1] - picture[i][c1-2]
-    print(avg//((r2-r1+1)*(c2-c1+1)))
+    print((picture[r2][c2] - picture[r2][c1-1] - picture[r1-1][c2] + picture[r1-1][c1-1])//((r2-r1+1)*(c2-c1+1)))
