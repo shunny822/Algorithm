763a6d0cd19e9fff2eb74d589ee0011d3c55a0d2
Suhyun Choe <sw92522@naver.com>
2024-06-14 11:10:25 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lines = [tuple(map(int, input().split())) for _ in range(n)]
+lines.sort()
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if lines[j][1] < lines[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n - max(dp))
