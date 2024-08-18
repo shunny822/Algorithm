4bf28f6f4cdbde58b614978494653d5129a0f8e8
Suhyun Choe <sw92522@naver.com>
2024-08-18 17:26:13 +0900

    [Silver II] Title: DNA 비밀번호, Time: 272 ms, Memory: 44624 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static boolean checkCnt(int A, int C, int G, int T, HashMap<Character, Integer> cnt) {
+	static int A, C, G, T;
+	
+	private static boolean checkCnt(HashMap<Character, Integer> cnt) {
 		if (cnt.get('A') < A || cnt.get('C') < C || cnt.get('G') < G || cnt.get('T') < T) {
 			return false;
 		}
@@ -23,10 +25,10 @@ public class Main {
 		String dna = br.readLine();
 		
 		StringTokenizer target = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(target.nextToken());
-		int C = Integer.parseInt(target.nextToken());
-		int G = Integer.parseInt(target.nextToken());
-		int T = Integer.parseInt(target.nextToken());
+		A = Integer.parseInt(target.nextToken());
+		C = Integer.parseInt(target.nextToken());
+		G = Integer.parseInt(target.nextToken());
+		T = Integer.parseInt(target.nextToken());
 		
 		HashMap<Character, Integer> count = new HashMap<>();
 		count.put('A', 0);
@@ -41,7 +43,7 @@ public class Main {
 		
 		int res = 0;
 		
-		if (checkCnt(A, C, G, T, count)) {
+		if (checkCnt(count)) {
 			res++;
 		}
 		
@@ -51,7 +53,7 @@ public class Main {
 			char addS = dna.charAt(i);
 			count.replace(addS, count.get(addS)+1);
 			
-			if (checkCnt(A, C, G, T, count)) {
+			if (checkCnt(count)) {
 				res++;
 			}
 		}
